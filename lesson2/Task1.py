7d699eb9abc52f587a3ade0724ffc3eafba1e6e5
mashkinaa99 <82476422+mashkinaa99@users.noreply.github.com>
2021-04-15 18:28:39 +0300

    Delete Task1.py

--- lesson2/Task1.py
+++
@@ -1,8 +0,0 @@
-name = 'Alena'
-day = '15 april'
-print(f'Good day {name}! {day} is a perfect day to learn some python.')
-
-x = 'Good day {}! {} is a perfect day to learn some python.'
-print(x.format(name, day))
-
-print('Good day %s! %s is a perfect day to learn some python.' % (name, day))
