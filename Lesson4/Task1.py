f538128d49fed2e118e786b6f64602f66e795cfb
Alena Mashkina <maskinaa99@gmail.com>
2021-04-21 22:18:41 +0300

    amending Task 1, 2, 4

--- Lesson4/Task1.py
+++ Lesson4/Task1.py
@@ -2,9 +2,8 @@ import random
 
 user_nmb = input('Guess the number from 1 to 10: ')
 numbers = random.randint(1, 10)
-while int(user_nmb) != numbers:
+if int(user_nmb) != numbers:
     print(f'You were close, but the correct answer {numbers}')
-    break
-if int(user_nmb) == numbers:
+else:
     print('Wow, you guessed it!')
 
--- Lesson4/Task2.py
+++ Lesson4/Task2.py
@@ -1,7 +1,4 @@
 name = input('Name: ')
 old = int(input('Old: '))
-
-while old == int(old):
-    old += 1
-    print(f'Hello {name}, on your next birthday you’ll be {old} years')
-    break
+old += 1
+print(f'Hello {name}, on your next birthday you’ll be {old} years')
