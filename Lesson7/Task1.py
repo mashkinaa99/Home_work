4423c78a7fdd5398b6fb842c1d252afdb1c3e5a0
Alena Mashkina <maskinaa99@gmail.com>
2021-04-27 18:37:19 +0300

    Add Task 1, 2, 3 to Lesson7

---
+++ Lesson7/Task1.py
@@ -0,0 +1,9 @@
+movie_name = '\'Harry Potter And The Chamber of secrets\''
+
+
+def favourite_movie(movie):
+    print(f'My favorite movie is {movie}')
+
+
+favourite_movie(movie_name)
+
---
+++ Lesson7/Task3.py
@@ -0,0 +1,6 @@
+def make_operation(plus, a, b, c):
+    if plus == '+':
+        print(f'Sum of all entered numbers = {a + b + c}')
+
+
+make_operation('+', 10, 5, 7)
