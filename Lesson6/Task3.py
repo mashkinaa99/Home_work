5872fae9b35bcc10944151709dc2c65f091d3ae7
Alena Mashkina <maskinaa99@gmail.com>
2021-04-28 15:51:38 +0300

    Edited Task3 to Lesson6

--- Lesson6/Task3.py
+++ Lesson6/Task3.py
@@ -1,8 +1,13 @@
 i = tuple(range(1, 11))
 j = tuple([x**2 for x in i])
-
 v = [i, j]
 
-print(v)
+
+list = [[row[i] for row in v] for i in range(len(i))]
+
+new_list = tuple(list)
+
+print(new_list)
+
 
 
