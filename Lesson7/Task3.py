449e8b2bed63c5bf38219226e4f69cc1a1fcabda
Alena Mashkina <maskinaa99@gmail.com>
2021-04-28 23:34:07 +0300

    Edited Task3 to Lesson7

--- Lesson7/Task3.py
+++ Lesson7/Task3.py
@@ -1,6 +1,22 @@
-def make_operation(plus, a, b, c):
-    if plus == '+':
-        print(f'Sum of all entered numbers = {a + b + c}')
+def make_operation(sign, *args):
+    plus = 0
+    multiplication = 1
+    minus = args[0]
+
+    if sign == '+':
+        for n in args:
+            plus += n
+        print(f'Sum of all entered numbers = {plus}')
+
+    if sign == '*':
+        for n in args:
+            multiplication *= n
+        print(f'Multiplication of all entered numbers = {multiplication}')
+
+    if sign == '-':
+        for n in args[1:]:
+            minus -= n
+        print(f'Subtracting all entered numbers = {minus}')
 
 
-make_operation('+', 10, 5, 7)
+make_operation('-', 10, 2, 6)
